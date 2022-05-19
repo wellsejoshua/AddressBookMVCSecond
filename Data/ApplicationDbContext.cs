481cef203cb3934f7133b05c1eaf4ca1f9f524ee
wellsejoshua <84493818+wellsejoshua@users.noreply.github.com>
2022-05-19 14:03:29 -0500

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using AddressBookMVCSecond.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBookMVCSecond.Data
+{
+  public class ApplicationDbContext : DbContext
+  {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+
+    public DbSet<Contact> Contacts { get; set; }
+  }
+}
